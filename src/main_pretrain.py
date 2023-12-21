64b6e3384c711dd7d849976d9c7d8bee6dfea448
fangzel <48748617+fangzel@users.noreply.github.com>
2023-12-20 23:54:04 -0800

    Update main_pretrain.py
    Signed-off-by: fangzel <48748617+fangzel@users.noreply.github.com>

--- src/main_pretrain.py
+++ src/main_pretrain.py
@@ -11,7 +11,7 @@ from .external.ase_dataset import AseReadDataset
 from .trainer_pretrain import TrainerPreTrain
 
 import sys
-sys.path.append('/crystal-transformer')
+sys.path.append('/CrystalGenerativeModels')
 from equiformer_v2.nets import EquiformerV2
 
 
